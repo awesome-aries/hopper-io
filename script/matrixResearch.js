e844147f724ea90d5fe589fb7acfab279bb93b84
Cara Takemoto <cktakemoto@gmail.com>
2019-06-27 12:48:43 -0400

    feat(flood-fill) modifying getSurroundSquares

--- script/matrixResearch.js
+++ script/matrixResearch.js
@@ -23,25 +23,37 @@ export const transformPxToCoords = (
   // TODO
 };
 
-export const setPath = (prevCartPos, nextCartPos, tileMatrix) => {
-  // cartPos = {x: 1, y: 1}
+export const setPath = (
+  prevCartPos,
+  nextCartPos,
+  tileMatrix,
+  exitPoint,
+  entryPoint
+) => {
+  // prevCartPos = {x: 1, y: 1}   -- the tile the cart was on before
+  // nextCartPos = {x: 1, y: 1}   -- the new tile the cart is now on
   // tileMatrix, an array of arrays where
   // 0 = unowned tile
   // 1 = tile is path
   // 2 = a filled in area, part of the harbor
 
   // Sets the current tile that the user is on as the path and sets the entry and exit points
+  let prevVal = tileMatrix[prevCartPos.x][prevCartPos.y];
+  let nextVal = tileMatrix[nextCartPos.x][nextCartPos.y];
 
-  // If the user is moving from harbor to sea (1 to 0), then we must set the exit point
-
-  // If the user is moving
-
-  tileMatrix[cartPos.x][cartPos.y] = 1;
+  // If the user is moving from harbor to sea (2 to 0), then we must set the exit point
+  if (prevVal === 2 && nextVal === 0) {
+    exitPoint = nextCartPos;
+  } else if (prevVal === 0 && nextVal === 2) {
+    // If the user is moving from sea to harbor (0 to 2), then we must set the entry point
+    entryPoint = nextCartPos;
+  }
+  tileMatrix[nextCartPos.x][nextCartPos.y] = 1;
 };
 
-export const getSurroundingSquares = (tile, tileValue, boardDimensions) => {
-  // tile, an array of arrays representing the plane
-  // boardDimensions = Ex: {x: 10, y: 10 }
+// eslint-disable-next-line complexity
+export const getSurroundingSquares = (tile, tileMatrix) => {
+  // tileMatrix, an array of arrays representing the plane
   // tile = {x, y}
 
   //get range of the surrounding square coordinates
@@ -52,10 +64,26 @@ export const getSurroundingSquares = (tile, tileValue, boardDimensions) => {
   let xmin = tile.x - 1 < 0 ? 0 : tile.x - 1;
   let ymin = tile.y - 1 < 0 ? 0 : tile.y - 1;
 
+  let boardDimensions = {
+    x: tileMatrix.length,
+    y: tileMatrix[0].length
+  };
+  let tileValue = tileMatrix[tile.x][tile.y];
+
   let xmax = tile.x + 1 >= boardDimensions ? boardDimensions - 1 : tile.x + 1;
   let ymax = tile.y + 1 >= boardDimensions ? boardDimensions - 1 : tile.y + 1;
 
-  return {xmin: xmin, xmax: xmax, ymin: ymin, ymax: ymax};
+  let range = {xmin: xmin, xmax: xmax, ymin: ymin, ymax: ymax};
+  let tiles = [];
+
+  for (var i = range.xmin; i <= range.xmax; i++) {
+    for (var j = range.ymin; j <= range.ymax; j++) {
+      if (tileValue === 0 || (tileValue === 1 && tileMatrix[i][j] === 1)) {
+        tiles.push({x: i, y: j});
+      }
+    }
+  }
+  return tiles;
 };
 
 export const floodFillArea = (tileMatrix, startPoint) => {
