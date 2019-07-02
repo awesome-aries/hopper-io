d6b136ce4495fc7c93126cd0697a48c6b175d7d4
Nida Jabbar <nj2296@columbia.edu>
2019-07-02 16:32:06 -0400

    feat(components/Welcome): input field for player name

--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
 const Welcome = props => {
-  return <div className="welcome">{/* eventually put stuff here */}</div>;
+  return (
+    <div className="welcome">
+      {/* eventually put stuff here */}
+      <form>
+        <label htmlFor="name">Player Name</label>
+        <input name="name" />
+      </form>
+    </div>
+  );
 };
+
+export default Welcome;
