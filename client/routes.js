a9bbc10cca4fee67923dbdfb2a2c5682c3df7edc
Cara Takemoto <cktakemoto@gmail.com>
2019-07-02 16:20:36 -0400

    feat(welcome): imported welcom screen to routes

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {withRouter, Route, Switch} from 'react-router-dom';
 import PropTypes from 'prop-types';
-import {Login, Signup, UserHome, GameView} from './components';
+import {Login, Signup, UserHome, GameView, Welcome} from './components';
 import {me} from './store';
 import Container from '@material-ui/core/Container';
 
