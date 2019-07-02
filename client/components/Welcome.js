a6a9cd7f1f606aaca3f2463401612682e2fc778d
Nida Jabbar <nj2296@columbia.edu>
2019-07-02 16:36:28 -0400

    feat(components/Welcome): name input + play button

--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -4,10 +4,15 @@ const Welcome = props => {
   return (
     <div className="welcome">
       {/* eventually put stuff here */}
-      <form>
-        <label htmlFor="name">Player Name</label>
-        <input name="name" />
-      </form>
+      <div>
+        <form>
+          <label htmlFor="name">Player Name</label>
+          <input name="name" />
+        </form>
+      </div>
+      <div>
+        <button type="submit">PLAY</button>
+      </div>
     </div>
   );
 };
