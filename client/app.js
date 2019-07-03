a902431057fc6e574f97f565b52e67fbc0c444d3
Nida Jabbar <nj2296@columbia.edu>
2019-07-03 12:36:12 -0400

    feat(components/App and client/app): update app file to render the Welcome & Game View

--- client/app.js
+++ client/app.js
@@ -1,33 +1,33 @@
-import React from 'react';
+// import React from 'react';
 
-import {Navbar} from './components';
-import Routes from './routes';
-import {makeStyles} from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Footer from './components/Footer';
+// import {Navbar} from './components';
+// import Routes from './routes';
+// import {makeStyles} from '@material-ui/core/styles';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import Footer from './components/Footer';
 
-const useStyles = makeStyles(theme => ({
-  app: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100vh'
-  },
-  main: {
-    marginTop: theme.spacing(8),
-    marginBottom: theme.spacing(2)
-  }
-}));
+// const useStyles = makeStyles(theme => ({
+//   app: {
+//     display: 'flex',
+//     flexDirection: 'column',
+//     minHeight: '100vh'
+//   },
+//   main: {
+//     marginTop: theme.spacing(8),
+//     marginBottom: theme.spacing(2)
+//   }
+// }));
 
-const App = () => {
-  const classes = useStyles();
-  return (
-    <div id="app" className={classes.app}>
-      <CssBaseline />
-      <Navbar />
-      <Routes className={classes.main} />
-      <Footer />
-    </div>
-  );
-};
+// const App = () => {
+//   const classes = useStyles();
+//   return (
+//     <div id="app" className={classes.app}>
+//       <CssBaseline />
+//       <Navbar />
+//       {/* <Routes className={classes.main} /> */}
+//       <Footer />
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;
--- client/components/App.js
+++ client/components/App.js
@@ -1,29 +1,52 @@
-/*
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 import Welcome from './Welcome';
 import GameView from './GameView';
+import {makeStyles} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const useStyles = makeStyles(theme => ({
+  app: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh'
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2)
+  }
+}));
 
 class App extends Component {
   render() {
+    // const classes = useStyles();
     const {isPlaying} = this.props;
     return (
       <div>
-        {!isPlaying && <Welcome />}
-        {isPlaying && <GameView />}
+        <div id="app">
+          <CssBaseline />
+          <Navbar />
+          {/* <Routes className={classes.main} /> */}
+          <Footer />
+        </div>
+        <div>
+          {!isPlaying && <Welcome />}
+          {isPlaying && <GameView />}
+        </div>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  isPlaying: state.reducer.isPlaying
+  isPlaying: state.gameState.isPlaying
 });
 
-const mapDispatchToProps = dispatch => ({
-  // thunk: () => dispatch(thunk())
-});
+// const mapDispatchToProps = dispatch => ({
+//   // thunk: () => dispatch(thunk())
+// });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-*/
+export default connect(mapStateToProps)(App);
