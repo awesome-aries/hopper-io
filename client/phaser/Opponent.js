6a50f167f59c4c2cf56dc663ffffb0b915edd577
Allison Geismar <ageismar213@gmail.com>
2019-07-08 14:55:41 -0400

    fix(opponent): can now see opponent sprite change direction

--- client/phaser/Opponent.js
+++ client/phaser/Opponent.js
@@ -11,62 +11,9 @@ export default class Opponent {
       .sprite(x, y, 'ship', 0)
       .setSize(50, 50);
 
-    // create animation
-    const anims = this.scene.anims;
-    anims.create({
-      key: 'ship-north',
-      frames: [{key: 'ship', frame: 0}],
-      frameRate: 3,
-      repeat: -1
-    });
-
-    anims.create({
-      key: 'ship-east',
-      frames: [{key: 'ship', frame: 1}],
-      frameRate: 3,
-      repeat: -1
-    });
-    anims.create({
-      key: 'ship-south',
-      frames: [{key: 'ship', frame: 2}],
-      frameRate: 3,
-      repeat: -1
-    });
-    anims.create({
-      key: 'ship-west',
-      frames: [{key: 'ship', frame: 3}],
-      frameRate: 3,
-      repeat: -1
-    });
-
     // set these as properties on the sprite so its accessible in the game
     this.sprite.socketId = socketId;
     this.sprite.direction = direction;
     // this.sprite.move = this.move
   }
-
-  update() {
-    if (this.sprite.direction === 'north') {
-      this.sprite.anims.play('ship-north');
-    } else if (this.sprite.direction === 'west') {
-      this.sprite.anims.play('ship-west');
-    } else if (this.sprite.direction === 'east') {
-      this.sprite.anims.play('ship-east');
-    } else if (this.sprite.direction === 'south') {
-      this.sprite.anims.play('ship-south');
-    }
-    // this.sprite.anims.play(`ship-${this.sprite.direction}`);
-  }
-  // destroy() {
-  //   this.sprite.destroy();
-  // }
-  // move(x, y, direction) {
-  //   console.log('move');
-  //   console.log('this.sprite', this.sprite);
-  //   // this.sprite.x = x;
-  //   // this.sprite.y = y;
-  //   this.sprite.setPosition(x, y);
-
-  //   this.sprite.direction = direction;
-  // }
 }
