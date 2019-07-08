646678ecee4ec5ce66a735973e9cc985d22bd23a
Katie Guest <katieguest11@gmail.com>
2019-07-08 18:22:02 -0400

    added playerCleared method to change previous tile to regular

--- server/game/tile.js
+++ server/game/tile.js
@@ -10,16 +10,27 @@ class Tile {
       color: initColor,
       type: initType
     };
+    this.regularTile = {
+      color: initColor,
+      type: initType
+    };
   }
   //if tile changes, update previous to be current and set current to be new type
   tileChange(color, type) {
     //if the type is different from current type, update both current and previous type
-    if (this.current.type !== type) {
+    //if we're switching from a current type = path to incoming type = harbor, we want to set previous type to regular (always)
+    if (type === 'harbor') {
+      this.previous = {...this.regularTile};
+    } else if (this.current.type !== type) {
       this.previous = {...this.current};
       this.current = {color, type};
     } else if (this.current.type === type) {
       //if the type is not different, just update current
       this.current = {color, type};
     }
+  }
+  playerCleared(color, type) {
+    //if a player existed on this tile it changes the previous tile value to be a regular tile if that player is killed
+    this.previous = {color, type};
   }
 }
